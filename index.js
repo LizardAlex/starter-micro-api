45ca14bfdc0846927c71f7930e1033fb09bb38a1
LizardAlex <alexandr-91@bk.ru>
2023-03-11 18:50:08 +0300

    u

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('example.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT, amount INTEGER)");
+
+
+    db.run("INSERT INTO lorem (info, amount) VALUES ('Zalue2p', 150)");
+
+
+    db.each("SELECT rowid AS id, info, amount FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info + " " + row.amount);
+    });
+});
+
+//db.close();
+
+
+
+
+
 var statsObject = {};
 
 if (fs.existsSync('./stat.json')) {
@@ -14,7 +35,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
 	console.log('server request');
 	console.log(req.url, req.method);
-    const split = req.url.split("?");
+/*    const split = req.url.split("?");
 	if (split[1]) {
 		const split1 = split[0].split("=");
 		const split2 = split[1].split("=");
@@ -26,11 +47,28 @@ const server = http.createServer((req, res) => {
 				error ? console.log(error) : null;
 			});
 		}
-	}
+	}*/
+	db.run(`INSERT INTO lorem (info, amount) VALUES ('Zalue2p', ${Math.floor(Math.random() * 150)})`);
+
 
 	res.setHeader('Content-type', 'application/json');
-	res.write(JSON.stringify(statsObject));
-	res.end();
+	let str = '';
+//	db.serialize(() => {
+    db.each("SELECT rowid AS id, info, amount FROM lorem", (err, row) => {
+        str += row.id + ": " + row.info + " " + row.amount;
+    }, () => {
+	    res.write(str);
+	    res.end();
+    });
+
+	//});
+	//db.close();
+    
+
+
+	
+	//res.write(JSON.stringify(statsObject));
+	
 });
 
 server.listen(PORT, 'localhost', (error) => {
