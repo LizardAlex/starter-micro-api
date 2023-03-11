ba48c5875cae3e4534abf530436a88ce22d595f1
LizardAlex <alexandr-91@bk.ru>
2023-03-11 14:13:42 +0300

    u

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+var statsObject = {};
+
+if (fs.existsSync('./stat.txt')) {
+	fs.readFile('./stat.txt', 'utf8', (error, data) => {
+        statsObject = JSON.parse(data);
+	});
+}
+
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+	console.log('server request');
+	console.log(req.url, req.method);
+    const split = req.url.split("?");
+	if (split[1]) {
+		const split1 = split[0].split("=");
+		const split2 = split[1].split("=");
+		if (split1[1] && split2[1]) {
+			if (!statsObject[split1[1]]) statsObject[split1[1]] = {};
+			if (!statsObject[split1[1]][split2[1]]) statsObject[split1[1]][split2[1]] = 0;
+			statsObject[split1[1]][split2[1]] += 1;
+			fs.writeFile('./stat.txt', JSON.stringify(statsObject), (error) => {
+				error ? console.log(error) : null;
+			});
+		}
+	}
+
+	res.setHeader('Content-type', 'application/json');
+	res.write(JSON.stringify(statsObject));
+	res.end();
+});
+
+server.listen(PORT, 'localhost', (error) => {
+	error ? console.log(error) : console.log('listening 300');
+});
